a1e87a896c4702388d433db82e9e5ecb8be1950d
An1310 <tung.an@ggg.com.vn>
2024-01-08 16:53:32 +0700

    Add code page customer-information

--- src/components/customer-infomation/LineInfoTab.tsx
+++ src/components/customer-infomation/LineInfoTab.tsx
@@ -16,95 +16,97 @@ const LineInfoTab: React.FC = ({ dataInfo, setDataInfo }) => {
     <>
       <Form className={'form-search-customer'} name='advanced_search'>
         <fieldset>
-          <legend>Line information</legend>
+          <legend><span className={"legend-color"}>Line information</span></legend>
           {/*line 1*/}
-          <Row gutter={24} style={{ marginBottom: '30px' }}>
-            <Col span={8}>
+          <Row gutter={24} style={{ marginLeft: '10px' }}>
+            <Col span={12}>
               <FormSelect
                 data={dataInfo}
                 setData={setDataInfo}
                 attribute={''}
-                title={'Tipo de cliente'}
-                isDisabled={true}
+                title={'Total quantity of lines'}
+                isRequired={true}
               />
             </Col>
           </Row>
 
           <Form className={'form-search-customer'} name='advanced_search'>
             <fieldset>
-              <legend>Group 1</legend>
-              {/*line 1*/}
-              <Row gutter={24} style={{ marginBottom: '30px' }}>
-                <Col span={8}>
-                  <FormSelect
-                    data={dataInfo}
-                    setData={setDataInfo}
-                    attribute={''}
-                    title={'Tipo de cliente'}
-                    isDisabled={true}
-                  />
-                </Col>
-                <Col span={8}>
-                  <FormSelect
-                    data={dataInfo}
-                    setData={setDataInfo}
-                    attribute={''}
-                    title={'Tipo de cliente'}
-                    isDisabled={true}
-                  />
-                </Col>
-              </Row>
+              <legend><span className={"legend-color"}>Group 1</span></legend>
+              <div style={{ marginLeft: "10px"}}>
+                {/*line 1*/}
+                <Row gutter={24} style={{ marginBottom: '30px' }}>
+                  <Col span={8}>
+                    <FormSelect
+                        data={dataInfo}
+                        setData={setDataInfo}
+                        attribute={''}
+                        title={'Plan'}
+                        isRequired={true}
+                    />
+                  </Col>
+                  <Col span={8}>
+                    <FormSelect
+                        data={dataInfo}
+                        setData={setDataInfo}
+                        attribute={''}
+                        title={'Quantity of lines'}
+                        isRequired={true}
+                    />
+                  </Col>
+                </Row>
 
-              {/*line 2*/}
-              <Row gutter={24} style={{ marginBottom: '30px' }}>
-                <Col span={8}>
-                  <FormSelect
-                    data={dataInfo}
-                    setData={setDataInfo}
-                    attribute={''}
-                    title={'Tipo de cliente'}
-                    isDisabled={true}
-                  />
-                </Col>
-                <Col span={8}>
-                  <FormSelect
-                    data={dataInfo}
-                    setData={setDataInfo}
-                    attribute={''}
-                    title={'Tipo de cliente'}
-                    isDisabled={true}
-                  />
-                </Col>
-              </Row>
+                {/*line 2*/}
+                <Row gutter={24} style={{ marginBottom: '30px' }}>
+                  <Col span={8}>
+                    <FormSelect
+                        data={dataInfo}
+                        setData={setDataInfo}
+                        attribute={''}
+                        title={'Reason of connection'}
+                        isRequired={true}
+                    />
+                  </Col>
+                  <Col span={8}>
+                    <FormSelect
+                        data={dataInfo}
+                        setData={setDataInfo}
+                        attribute={''}
+                        title={'Branch Assigned'}
+                        isRequired={true}
+                    />
+                  </Col>
+                </Row>
+              </div>
+
+              {/* tables */}
+              <Form className={'form-search-customer'} name='advanced_search'>
+                <fieldset>
+                  <legend style={{ marginBottom: "0 !important"}}><span className={"legend-color"}>Payment Information</span></legend>
+                  <Table
+                      rowKey={(record) => record.id}
+                      dataSource={[]}
+                      pagination={false}
+                      loading={tableLoading}
+                      locale={{ emptyText: <EmptyUI content={NO_DATA} /> }}
+                  >
+                    <Column
+                        title={''}
+                        dataIndex=''
+                        key=''
+                        // render={(type, record) => {
+                        //     return BOM_TYPE_DISPLAY[type];
+                        // }}
+                    />
+                    <Column title={'Quantity of lines'} dataIndex='' key='' />
+                    <Column title={'Unit Price'} dataIndex='' key='' />
+                    <Column title={'Totals Price'} dataIndex='material_type_label' key='material_type_label' />
+                    <Column title={'Fingerprint Validation'} dataIndex='' key='' />
+                  </Table>
+                </fieldset>
+              </Form>
             </fieldset>
           </Form>
-        </fieldset>
-      </Form>
-
-      <Form className={'form-search-customer'} name='advanced_search'>
-        <fieldset>
-          <legend>Payment Information</legend>
-
-          <Table
-            rowKey={(record) => record.id}
-            dataSource={[]}
-            pagination={false}
-            loading={tableLoading}
-            locale={{ emptyText: <EmptyUI content={NO_DATA} /> }}
-          >
-            <Column
-              title={''}
-              dataIndex=''
-              key=''
-              // render={(type, record) => {
-              //     return BOM_TYPE_DISPLAY[type];
-              // }}
-            />
-            <Column title={'Quantity of lines'} dataIndex='' key='' />
-            <Column title={'Unit Price'} dataIndex='' key='' />
-            <Column title={'Totals Price'} dataIndex='material_type_label' key='material_type_label' />
-            <Column title={'Fingerprint Validation'} dataIndex='' key='' />
-          </Table>
         </fieldset>
       </Form>
     </>
